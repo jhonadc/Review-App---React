a35583f5f33be898e310469a8b35fc95601c3d64
jhonadc <jhonathanaugusto@gmail.com>
2022-03-21 16:44:42 +0100

    spinner for loading created

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,18 @@
 import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem';
 import FeedbackContext from '../context/FeedbackContext';
-function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+import Spinner from '../components/shared/Spinner';
 
-  if (!feedback || feedback.length === 0) {
+function FeedbackList() {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback yet</p>;
   }
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className='feedback-list'>
       {feedback.map((item) => (
         <FeedbackItem key={item.id} item={item} />
